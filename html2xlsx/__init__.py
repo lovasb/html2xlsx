85f84c39171a8fa7284f24913beeb5c8a92c2853
Lovas Bence <lovas.bence@codehouse.hu>
2014-10-27 08:44:36 +0100

    asdf

--- html2xlsx/__init__.py
+++ html2xlsx/__init__.py
@@ -1,6 +1,6 @@
 __version__ = "0.2"
 
-from .sheet import Sheet
-from .row import Row
-from .cell import Cell
-from .table import Table
+#from .sheet import Sheet
+#from .row import Row
+#from .cell import Cell
+#from .table import Table
